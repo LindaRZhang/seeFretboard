aa52b84e254f1f81798d43a4d43f437d07d5ae0f
LindaRZhang <ronglindaz@gmail.com>
2023-05-02 01:56:56 -0400

    short groundtruth vs prediction

--- Examples/groundtruthVPredictionTest.py
+++ Examples/groundtruthVPredictionTest.py
@@ -6,5 +6,9 @@ from SeeFretboard import SeeFretboard
 
 fretboard = SeeFretboard("h", 6, 1, 12)
 fretboard.drawHorizontalFretboard()
-fretboard.addNotesAllString("0,0,2,2,0,0")
+
+
+fretboard.addNotesAllString("-1,0,2,2,0,0")
+fretboard.setNoteType("groundTruth")
+fretboard.addNotesAllString("-1,-1,5,2,0,0")
 fretboard.showFretboard()
