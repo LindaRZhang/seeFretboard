4eb316537a8fb4b834580e9b68a11c00e0e97145
LindaRZhang <ronglindaz@gmail.com>
2023-05-17 02:51:27 -0400

    edits, scale degree broken..

--- Examples/arpeggio.py
+++ Examples/arpeggio.py
@@ -7,11 +7,11 @@ from SeeFretboard import SeeFretboard
 fretboard = SeeFretboard("v", 1, 12)
 fretboard.drawVerticalFretboard()
 
-fretboard.getPitchCollection().setPitchesType("pitchesWithOctave")
-#fretboard.getPitchCollection().setPitchesType("pitchesScaleDegrees")
+#fretboard.getPitchCollection().setPitchesType("pitchesWithOctave")
+fretboard.getPitchCollection().setPitchesType("pitchesScaleDegrees")
 
-fretboard.addArpeggio("c","suspended-second")#gota put words for arpeggio kinds
+#fretboard.addArpeggio("c","suspended-second")#gota put words for arpeggio kinds
 #fretboard.addArpeggio("c") #just show 1 note or octaves
-#fretboard.addArpeggio("c","","P1 M3 P5 M7")
+fretboard.addArpeggio("c","","P1 M3 P5 M7")
 
 fretboard.showFretboard()
